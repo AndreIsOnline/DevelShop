058a3f2e01cd181bbe1d12ed0da5aa75d295ec87
Andrea <andreascorza85@gmail.com>
2022-10-03 13:58:55 +0200

    view store funzionante

--- ecommerce/src/main/java/kosmok/teamlebimbe/ecommerce/dao/ShoppingCartDao.java
+++ ecommerce/src/main/java/kosmok/teamlebimbe/ecommerce/dao/ShoppingCartDao.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Set;
 
 @Repository
@@ -14,14 +15,15 @@ public class ShoppingCartDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private IShoppingKart iShoppingKart;
+    public List<String> getStore(Long id) {
+        try {
+            return jdbcTemplate.queryForList("SELECT name FROM item WHERE " +
+                    "seller_id_seller_id = ?", String.class, id );
 
-    @Autowired
-    private IRegistrationCustomerRepository iRegistrationCustomerRepository;
+        } catch (Exception e) {
 
-
-
-
+        }
+        return null;
+    }
 
 }
